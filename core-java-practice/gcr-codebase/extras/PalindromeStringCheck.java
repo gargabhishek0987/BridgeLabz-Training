853d64ce6fdd23d5349a0f8a0fd449497af59771
gargabhishek0987 <gargabhishek742@gmial.com>
2025-12-23 12:43:50 +0530

    [Abhishek garg] Add. Solved extras  problems

---
+++ core-java-practice/gcr-codebase/extras/PalindromeStringCheck.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class PalindromeStringCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        String reverse = "";
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reverse += str.charAt(i);
+
+        }
+        if (str.equals(reverse)) {
+            System.out.println("The string is a palindrome.");
+
+        }
+        else {
+            System.out.println("The string is not a palindrome.");
+        }
+
+        
+    }
+    
+}
