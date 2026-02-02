558e583802267f26ed872179d57042c9cecb010c
gargabhishek0987 <gargabhishek742@gmial.com>
2026-02-02 13:05:12 +0530

    [Abhishek garg] add some files for scenario based problem workshop

---
+++ java-8-features-practice/gcr-codebase/SmartCityTransport/BusService.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+
+public class BusService {
+    List<String> service = new LinkedList<>();
+
+    public service(List<String> service){
+        this.service = service;
+
+    }
+
+
+    public class String schedules(string departureTime , String fare){
+        
+
+
+    }
+
+}
+0
---
+++ java-8-features-practice/gcr-codebase/SmartCityTransport/SmartCity.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class SmartCity {
+    String departureTime;
+    String fare;
+
+    public SmartCity(String departureTime , String fare){
+        this.departureTime = departureTime;
+        this.fare = fare ; 
+    }
+
+    
+}
+
+class TransportManagment {
+
+    
+
+
+    public static String BookaTrip(String departureTime , String fare){
+        List<String> record = new LinkedList<>();
+       
+    }
+     public static List<String> recordInfo(List<String> record ){
+        return record.stream()
+                     .filter( () ->{
+                        Comparable.comapreTo()
+
+                     })
+     }
+    
+}
+
